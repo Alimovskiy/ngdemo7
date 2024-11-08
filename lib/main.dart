5b44b875789b52e9072297e1a412b5b22a34fc78
MrDjonS <mrsammister3@gmail>
2024-11-08 14:01:53 +0500

    es-ko-zh added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
 import 'package:ngdemo7/pages/home_page.dart';
 
 void main() async {
@@ -8,7 +8,17 @@ void main() async {
 
   runApp(
     EasyLocalization(
-        supportedLocales: [Locale('en', 'US'), Locale('ru', 'RU'), Locale('uz', 'UZ'),  Locale('de', 'DE'),  Locale('ar', 'DZ'), Locale('fr', 'FR')],
+        supportedLocales: [
+          Locale('en', 'US'),
+          Locale('ru', 'RU'),
+          Locale('uz', 'UZ'),
+          Locale('de', 'DE'),
+          Locale('ar', 'DZ'),
+          Locale('fr', 'FR'),
+          Locale('es', 'ES'),
+          Locale('ko', 'KR'),
+          Locale('zh', 'CN'),
+        ],
         path: 'assets/translations', // <-- change the path of the translation files
         fallbackLocale: Locale('en', 'US'),
         child: MyApp()
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:ngdemo7/main.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -24,88 +23,146 @@ class _HomePageState extends State<HomePage> {
           children: [
             Expanded(
               child: Center(
-                child: Text('flutter'.tr(),style: TextStyle(fontSize: 20),),
+                child: Text(
+                  'flutter'.tr(),
+                  style: TextStyle(fontSize: 20),
+                ),
               ),
             ),
-            Row(
+            Column(
               children: [
-                Expanded(
-                  child: MaterialButton(
-                    height: 45,
-                    color: Colors.green,
-                    onPressed: () {
-                      context.setLocale(Locale('en', 'US'));
-                    },
-                    child: Text("English",style: TextStyle(color: Colors.white),),
-                  ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: MaterialButton(
+                        height: 45,
+                        color: Colors.grey[100],
+                        onPressed: () {
+                          context.setLocale(Locale('en', 'US'));
+                        },
+                        child: Text("English",style: TextStyle(color: Colors.white),),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Expanded(
+                      child: MaterialButton(
+                        height: 45,
+                        color: Colors.grey[200],
+                        onPressed: () {
+                          context.setLocale(Locale('ru', 'RU'));
+                        },
+                        child: Text("Russian",style: TextStyle(color: Colors.white),),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Expanded(
+                      child: MaterialButton(
+                        height: 45,
+                        color: Colors.grey[300],
+                        onPressed: () {
+                          context.setLocale(Locale('uz', 'UZ'));
+                        },
+                        child: Text("Uzbek",style: TextStyle(color: Colors.white),),
+                      ),
+                    ),
+                  ],
                 ),
+
                 SizedBox(
-                  width: 10,
+                  height: 10,
                 ),
-                Expanded(
-                  child: MaterialButton(
-                    height: 45,
-                    color: Colors.red,
-                    onPressed: () {
-                      context.setLocale(Locale('ru', 'RU'));
-                    },
-                    child: Text("Russian",style: TextStyle(color: Colors.white),),
-                  ),
+
+                Row(
+                  children: [
+                    Expanded(
+                      child: MaterialButton(
+                        height: 45,
+                        color: Colors.grey[400],
+                        onPressed: () {
+                          context.setLocale(Locale('ko', 'KR'));
+                        },
+                        child: Text("Korean",style: TextStyle(color: Colors.white),),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Expanded(
+                      child: MaterialButton(
+                        height: 45,
+                        color: Colors.grey[500],
+                        onPressed: () {
+                          context.setLocale(Locale('es', 'ES'));
+                        },
+                        child: Text("Spanish",style: TextStyle(color: Colors.white),),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Expanded(
+                      child: MaterialButton(
+                        height: 45,
+                        color: Colors.grey[600],
+                        onPressed: () {
+                          context.setLocale(Locale('zh', 'CN'));
+                        },
+                        child: Text("Chinese",style: TextStyle(color: Colors.white),),
+                      ),
+                    ),
+                  ],
                 ),
+
                 SizedBox(
-                  width: 10,
+                  height: 10,
                 ),
-                Expanded(
-                  child: MaterialButton(
-                    height: 45,
-                    color: Colors.orange,
-                    onPressed: () {
-                      context.setLocale(Locale('uz', 'UZ'));
-                    },
-                    child: Text("Uzbek",style: TextStyle(color: Colors.white),),
-                  ),
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                Expanded(
-                  child: MaterialButton(
-                    height: 45,
-                    color: Colors.yellow,
-                    onPressed: () {
-                      context.setLocale(Locale('de', 'DE'));
-                    },
-                    child: Text("Nemis",style: TextStyle(color: Colors.white),),
-                  ),
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                Expanded(
-                  child: MaterialButton(
-                    height: 45,
-                    color: Colors.blue,
-                    onPressed: () {
-                      context.setLocale(Locale('ar', 'DZ'));
-                    },
-                    child: Text("Arabic",style: TextStyle(color: Colors.white),),
-                  ),
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                Expanded(
-                  child: MaterialButton(
-                    height: 45,
-                    color: Colors.black,
-                    onPressed: () {
-                      context.setLocale(Locale('fr', 'FR'));
-                    },
-                    child: Text("Franch",style: TextStyle(color: Colors.white),),
-                  ),
+
+                Row(
+                  children: [
+                    Expanded(
+                      child: MaterialButton(
+                        height: 45,
+                        color: Colors.grey[700],
+                        onPressed: () {
+                          context.setLocale(Locale('de', 'DE'));
+                        },
+                        child: Text("Nemis",style: TextStyle(color: Colors.white),),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Expanded(
+                      child: MaterialButton(
+                        height: 45,
+                        color: Colors.grey[800],
+                        onPressed: () {
+                          context.setLocale(Locale('ar', 'DZ'));
+                        },
+                        child: Text("Arabic",style: TextStyle(color: Colors.white),),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Expanded(
+                      child: MaterialButton(
+                        height: 45,
+                        color: Colors.grey[900],
+                        onPressed: () {
+                          context.setLocale(Locale('fr', 'FR'));
+                        },
+                        child: Text("Franch",style: TextStyle(color: Colors.white),),
+                      ),
+                    ),
+                  ],
                 ),
               ],
-            ),
+            )
           ],
         ),
       ),
